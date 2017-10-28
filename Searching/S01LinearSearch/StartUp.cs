d37dac58ab183da60da82426905d77964349683a
dobri19 <dobri19@users.noreply.github.com>
2017-10-28 11:54:19 +0300

    Searching

---
+++ Searching/S01LinearSearch/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S01LinearSearch
+{
+    public class StartUp
+    {
+        /* 
+           Worst and average performance: O(n)
+           As a result, even though in theory other search algorithms may be faster than linear search 
+           (for instance binary search), in practice even on medium-sized arrays (around 100 items or less) 
+           it might be infeasible to use anything else. On larger arrays, it only makes sense to use other, 
+           faster search methods if the data is large enough, because the initial time to prepare (sort) 
+           the data is comparable to many linear searches [7]
+        */
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 6, 10, -4, 55, 3, 7 };
+            Console.WriteLine(LinearSearch(list, 10));
+        }
+
+        public static int LinearSearch(List<int> list, int x)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == x)
+                {
+                    return x;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Searching/S02BinarySearch/StartUp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace S02BinarySearch
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            /// Average performance	O(log n)
+            /// Auxiliary Space: O(1) in case of iterative implementation. In case of recursive implementation, 
+            /// O(Logn) recursion call stack space.
+            List<int> list = new List<int> { 1, 2, 3, 6, 10, -4, 55, 3, 7 };
+            list.Sort();
+            Console.WriteLine(BinarySearch(list, 10));
+        }
+
+        public static int BinarySearch(List<int> list, int element)
+        {
+            //return SearchRecursive(list, 0, list.Count - 1, element);
+            return SearchIterative(list, 0, list.Count - 1, element);
+        }
+
+        private static int SearchRecursive(List<int> list, int from, int to, int element)
+        {
+            if (list == null || list.Count == 0) 
+            {
+                return -1;
+            }
+
+            if (from == to)
+            {
+                if (list[from] == element)
+                {
+                    return element;
+                }
+
+                return -1;
+            }
+
+            var mid = (from + to) / 2;
+
+            if (list[mid] == element)
+            {
+                return element;
+            }
+
+            if (list[mid] > element)
+            {
+                return SearchRecursive(list, from, mid, element);
+            }
+
+            return SearchRecursive(list, mid + 1, to, element);
+        }
+
+        private static int SearchIterative(List<int> list, int from, int to, int element)
+        {
+            while (from <= to)
+            {
+                if (list == null || list.Count == 0)
+                {
+                    return -1;
+                }
+
+                if (from == to)
+                {
+                    if (list[from] == element)
+                    {
+                        return element;
+                    }
+
+                    return -1;
+                }
+
+                var mid = (from + to) / 2;
+
+                if (list[mid] == element)
+                {
+                    return element;
+                }
+
+                if (list[mid] > element)
+                {
+                    to = mid - 1;
+                }
+                else
+                {
+                    from = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
